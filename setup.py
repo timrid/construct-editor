e3ac8e3d859e961ccfc1ef94adffea2a347cf8b6
Tim Rid <6593626+timrid@users.noreply.github.com>
2021-04-14 20:35:19 +0200

    reduce wxPython dependency to >=4.1.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         "construct==2.10.66",
         "construct-typing==0.4.1",
-        "wxPython>=4.1.1",
+        "wxPython>=4.1.0",
         "arrow>=1.0.0"
     ],
     keywords=[
