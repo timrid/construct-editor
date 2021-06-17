45b5d716cd69fc70592e87434c7283a957a64754
Tim Rid <6593626+timrid@users.noreply.github.com>
2021-06-17 20:13:35 +0200

    changed test_greedyrange, so that the cs.GreedyRange is at root level, to test "List View" at root level

--- construct_editor/gallery/test_greedyrange.py
+++ construct_editor/gallery/test_greedyrange.py
@@ -6,24 +6,19 @@ from . import GalleryItem
 
 
 @dataclasses.dataclass
-class GreedyRangeTest(cst.DataclassMixin):
-    @dataclasses.dataclass
-    class Entry(cst.DataclassMixin):
-        id: int = cst.csfield(cs.Int8sb)
-        width: int = cst.csfield(cs.Int8sb)
-        height: int = cst.csfield(cs.Int8sb)
-
-    entries: t.List[Entry] = cst.csfield(cs.GreedyRange(cst.DataclassStruct(Entry)))
-    cnt: int = cst.csfield(cs.Computed(lambda ctx: len(ctx.entries)))
+class Entry(cst.DataclassMixin):
+    id: int = cst.csfield(cs.Int8sb)
+    width: int = cst.csfield(cs.Int8sb)
+    height: int = cst.csfield(cs.Int8sb)
 
 
-constr = cst.DataclassStruct(GreedyRangeTest)
+constr = cs.GreedyRange(cst.DataclassStruct(Entry))
 
 gallery_item = GalleryItem(
     construct=constr,
     example_binarys={
-        "Zeros": bytes([]),
-        "1": bytes([1, 10, 10]),
         "5": bytes([1, 10, 10, 2, 10, 10, 3, 18, 18, 4, 10, 10, 5, 20, 20]),
+        "1": bytes([1, 10, 10]),
+        "Zeros": bytes([]),
     },
 )
