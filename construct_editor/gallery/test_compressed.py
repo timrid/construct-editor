9c1c5a7273c5e11d19b01be85e7762fa68e0ffe7
Tim Rid <6593626+timrid@users.noreply.github.com>
2021-07-25 13:47:42 +0200

    - Reordered constructs in "include_metadata()" - added cs.BytesSwapped subcon to "test_compressed"

--- construct_editor/gallery/test_compressed.py
+++ construct_editor/gallery/test_compressed.py
@@ -29,6 +29,14 @@ constr = cs.Struct(
                     "bits2" / cs.BitsInteger(3),
                     "bits3" / cs.BitsInteger(6),
                 ),
+                "swapped_bits"
+                / cs.ByteSwapped(
+                    cs.BitStruct(
+                        "bits1" / cs.BitsInteger(7),
+                        "bits2" / cs.BitsInteger(3),
+                        "bits3" / cs.BitsInteger(6),
+                    )
+                ),
             ),
             "zlib",
         ),
@@ -38,7 +46,7 @@ constr = cs.Struct(
 # b = constr.build(
 #     dict(
 #         compressed_bits=dict(value1=0, value2=1, value3=3),
-#         compressed_bytes=dict(value1=0, value2=1, value3=3, bits=dict(bits1=0, bits2=1, bits3=3),),
+#         compressed_bytes=dict(value1=0, value2=1, value3=3, bits=dict(bits1=0, bits2=1, bits3=3),swapped_bits=dict(bits1=0, bits2=1, bits3=3),),
 #     ),
 # )
 # print(b.hex())
@@ -46,6 +54,6 @@ constr = cs.Struct(
 gallery_item = GalleryItem(
     construct=constr,
     example_binarys={
-        "1": bytes.fromhex("0a789c63700600004500440e789c636064606670060000570048"),
+        "1": bytes.fromhex("0a789c637006000045004410789c63606460667076660000016d008b"),
     },
 )
--- construct_editor/helper/preprocessor.py
+++ construct_editor/helper/preprocessor.py
@@ -98,6 +98,7 @@ class IncludeGuiMetaData(cs.Subconstruct):
         return obj
 
 
+# #############################################################################
 def include_metadata(
     constr: "cs.Construct[t.Any, t.Any]", nested_stream_ctr: int = 0
 ) -> "cs.Construct[t.Any, t.Any]":
@@ -105,6 +106,8 @@ def include_metadata(
     Surrond all named entries of a construct with offsets, so that
     we know the offset in the byte-stream and the length
     """
+
+    # ########## Simple Constructs ############################################
     if isinstance(
         constr,
         (
@@ -126,6 +129,27 @@ def include_metadata(
     ):
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
+    # ########## Simple Subconstructs #########################################
+    elif isinstance(
+        constr,
+        (
+            cs.Const,
+            cs.Rebuild,
+            cs.Default,
+            cs.Padded,
+            cs.Aligned,
+            cs.Pointer,
+            cs.Peek,
+            cst.DataclassStruct,
+            cs.Array,
+            cs.GreedyRange,
+        ),
+    ):
+        constr = copy.copy(constr)  # constr is modified, so we have to make a copy
+        constr.subcon = include_metadata(constr.subcon, nested_stream_ctr)  # type: ignore
+        return IncludeGuiMetaData(constr, nested_stream_ctr)
+
+    # ########## Stream Manipulating Subconstructs ############################
     elif isinstance(
         constr,
         (
@@ -143,7 +167,8 @@ def include_metadata(
         constr.subcon = include_metadata(constr.subcon, nested_stream_ctr + 1)  # type: ignore
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
-    elif isinstance(constr, (cs.Struct)):
+    # Struct ##################################################################
+    elif isinstance(constr, cs.Struct):
         constr = copy.copy(constr)  # constr is modified, so we have to make a copy
         new_subcons = []
         for subcon in constr.subcons:
@@ -151,17 +176,14 @@ def include_metadata(
         constr.subcons = new_subcons
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
-    elif isinstance(constr, (cs.Array, cs.GreedyRange)):
-        constr = copy.copy(constr)  # constr is modified, so we have to make a copy
-        constr.subcon = include_metadata(constr.subcon, nested_stream_ctr)
-        return IncludeGuiMetaData(constr, nested_stream_ctr)
-
+    # IfThenElse ##############################################################
     elif isinstance(constr, cs.IfThenElse):
         constr = copy.copy(constr)  # constr is modified, so we have to make a copy
         constr.thensubcon = include_metadata(constr.thensubcon, nested_stream_ctr)
         constr.elsesubcon = include_metadata(constr.elsesubcon, nested_stream_ctr)
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
+    # Switch ##################################################################
     elif isinstance(constr, cs.Switch):
         constr = copy.copy(constr)  # constr is modified, so we have to make a copy
         new_cases = {}
@@ -172,28 +194,13 @@ def include_metadata(
             constr.default = include_metadata(constr.default, nested_stream_ctr)
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
-    elif isinstance(
-        constr,
-        (
-            cs.Renamed,
-            cs.Const,
-            cs.Rebuild,
-            cs.Default,
-            cs.Padded,
-            cs.Aligned,
-            cs.Pointer,
-            cs.Peek,
-        ),
-    ):
+    # Renamed #################################################################
+    elif isinstance(constr, cs.Renamed):
         constr = copy.copy(constr)  # constr is modified, so we have to make a copy
         constr.subcon = include_metadata(constr.subcon, nested_stream_ctr)  # type: ignore
         return constr
 
-    elif isinstance(constr, cst.DataclassStruct):
-        constr = copy.copy(constr)  # constr is modified, so we have to make a copy
-        constr.subcon = include_metadata(constr.subcon, nested_stream_ctr)  # type: ignore
-        return IncludeGuiMetaData(constr, nested_stream_ctr)
-
+    # Misc ####################################################################
     elif isinstance(
         constr,
         (
@@ -210,7 +217,7 @@ def include_metadata(
             type(cs.Tell),
             type(cs.Pass),
             type(cs.Terminated),
-        ),  # type: ignore
+        ),
     ):
         return IncludeGuiMetaData(constr, nested_stream_ctr)
 
