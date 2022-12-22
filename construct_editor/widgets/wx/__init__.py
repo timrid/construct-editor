866144ac5bc0f4b1e026f25dffce2b9f311033cc
timrid <6593626+timrid@users.noreply.github.com>
2022-12-22 16:47:47 +0100

    added "WxPythonCodeEditor" for later use

--- construct_editor/widgets/wx/__init__.py
+++ construct_editor/widgets/wx/__init__.py
@@ -1,5 +1,11 @@
 from .wx_construct_editor import WxConstructEditor
 from .wx_construct_hex_editor import WxConstructHexEditor
 from .wx_hex_editor import WxHexEditor
+from .wx_python_code_editor import WxPythonCodeEditor
 
-__all__ = ["WxConstructEditor", "WxConstructHexEditor", "WxHexEditor"]
+__all__ = [
+    "WxConstructEditor",
+    "WxConstructHexEditor",
+    "WxHexEditor",
+    "WxPythonCodeEditor",
+]
